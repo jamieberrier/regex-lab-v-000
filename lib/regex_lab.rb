ea62d2d412c73a22613588f36f549b3951be193d
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 18:23:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,11 @@
 require 'pry'
 def starts_with_a_vowel?(word)
   binding.pry
-  if
-
-  else
-
-  end
+  # if
+  # 
+  # else
+  # 
+  # end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)

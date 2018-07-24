ae59eb2216946f7b862f8ec44fce2e239a16c119
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 19:41:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,12 +16,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  binding.pry
-  # if text.match(/\^[A-Z].*\W$/)
-  #   true
-  # else
-  #   false
-  # end
+  #binding.pry
+  if text.match(/\^[A-Z].*\W$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)

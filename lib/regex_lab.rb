2deeb464457195a7015bcf52dab89a6451f2055f
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 18:48:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,5 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  #binding.pry
   if word.match(/\b[aeiou]/i)
     true
   else
@@ -9,7 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  binding.pry
 end
 
 def words_five_letters_long(text)

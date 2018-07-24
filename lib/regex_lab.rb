58b8358f13f0cded411b04531fe6786beb7ce4a6
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 18:57:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   binding.pry
 
-  text.scan(/\A(un)/)
+  text.scan(/\A(un)/) && text.scan(/\z(ing)/)
 end
 
 def words_five_letters_long(text)

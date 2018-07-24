84bc9071ac269cadf918cf24987b1a8424bc6ff9
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 20:09:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,9 +25,9 @@ end
 
 def valid_phone_number?(phone)
  #binding.pry
-  if !phone.match(/^([0-9]{10})$/)
-    false
-  else
+  if phone.match(/^([0-9]{10})$/) != nil
     true
+  else
+    false
   end
 end

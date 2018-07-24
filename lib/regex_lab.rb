bc8b268202e4e5d85ed37a8c411c546b1e9c4100
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 19:37:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,11 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   #binding.pry
-  text.match()
+  if text.match(//)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)

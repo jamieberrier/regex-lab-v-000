9faab63c93a9ec15ed7ecfc095b8160999a158b4
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 19:33:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,11 +13,10 @@ end
 
 def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
-  #binding.pry
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  #binding.pry
 end
 
 def valid_phone_number?(phone)
